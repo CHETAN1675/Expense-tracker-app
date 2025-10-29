592c91a079546eb5b3fc001fb7b836cb97f55736
CHETAN1675 <mahajanchetan001@gmail.com>
2025-10-29 17:13:18 +0530

    complete profile

--- src/components/Profile/ProfileForm.jsx
+++ src/components/Profile/ProfileForm.jsx
@@ -1,50 +1,77 @@
 import { useRef, useContext } from "react";
-
 import AuthContext from "../../store/AuthContext";
 import classes from "./ProfileForm.module.css";
+import { useNavigate } from "react-router-dom";
 
 const ProfileForm = () => {
-  const newPasswordInputRef = useRef();
+  const fullNameInputRef = useRef();
+  const profileUrlInputRef = useRef();
   const authCtx = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  const submitHandler = (event) => {
+  const cancelHandler = ()=>{
+    navigate("/");
+  };
+
+  
+
+  const updateProfile = async (event) => {
     event.preventDefault();
 
-    const enteredNewPassword = newPasswordInputRef.current.value;
-
-    fetch(
+    const enteredName = fullNameInputRef.current.value;
+    const enteredPhotoUrl = profileUrlInputRef.current.value;
+try{
+    const response = await fetch(
       "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCprTZVW6b9fM51Dp_WL5C-T5yTGXa7t9s",
       {
         method: "POST",
-        body: JSON.stringify({
+        headers: { "Content-Type": "application/json"},
+         body: JSON.stringify({
           idToken: authCtx.token,
-          password: enteredNewPassword,
-          returnSecureToken: false,
+          displayName: enteredName,
+          photoUrl : enteredPhotoUrl,
+          returnSecureToken: true,
         }),
-        headers: {
-          "Content-Type:": "application/json",
-        },
       }
-    ).then((res) => {
-      //assumption: Always succeeds
-    });
+    );
+    
+    const data = await response.json();
+    console.log(data);
+
+    fullNameInputRef.current.value ="";
+    profileUrlInputRef.current.value = "";
+   } catch (error){
+      console.error(error);
+     }
   };
 
   return (
-    <form className={classes.form} onSubmit={submitHandler}>
+    <div>
+    <form className={classes.form} >
       <div className={classes.control}>
-        <label htmlFor="new-password">New Password</label>
+        <label htmlFor="full-name">Full Name</label>
         <input
-          type="password"
-          id="new-password"
-          minLength="7"
-          ref={newPasswordInputRef}
+          type="text"
+          id="full-name"
+          ref={fullNameInputRef} 
+          required
+        />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="photo-url">Profile Photo URL</label>
+        <input
+          type="text"
+          id="photo-url"
+          ref={profileUrlInputRef} 
+          required
         />
       </div>
       <div className={classes.action}>
-        <button>Change Password</button>
+        <button onClick={updateProfile} className={classes.update}>Update</button>
+         <button onClick={cancelHandler} className={classes.cancel}>Cancel</button>
       </div>
-    </form>
+     </form>
+    </div>
   );
 };
 
